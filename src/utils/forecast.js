312237e4864d37d7b66ae4c3e1777d0ef56b9305
01659879 <01659879@corp.root>
2019-04-08 18:26:23 +0800

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -23,7 +23,7 @@ const forecast = (latitude, longitude, callback) =>{
         } else if(body.error){
             callback('Data Invalid!', undefined)
         }else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' +body.currently.temperature + ' degrees out. There is a '+ body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' +body.currently.temperature + ' degrees out. The high today is '+ body.daily.data[0].temperatureHigh+ ' with a low of '+ body.daily.data[0].temperatureLow +'. There is a '+ body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
